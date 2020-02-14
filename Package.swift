ab6e89463db59698bb0a82e47f3893bfc2936ee0
misyobun <naoto0101@gmail.com>
2020-02-14 12:30:19 +0900

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MSBBarChart",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "MSBBarChart",
+            targets: ["MSBBarChart"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "MSBBarChart",
+            dependencies: [],
+            path: "MSBBarChart/Sources"),
+    ]
+)
